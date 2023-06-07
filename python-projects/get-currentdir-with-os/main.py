fa57b2ca28f0fc5f4d71bb294d9b3c312d3ef9bf
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-06-07 10:21:47 -0300

    finishing get_current_dir using os

--- python-projects/get-currentdir-with-os/main.py
+++ python-projects/get-currentdir-with-os/main.py
@@ -3,5 +3,7 @@ import os
 
 print(os.getcwd())
 
-
-
+"""
+(.venv) [develop][~/workspace/sandbox-projects/python-projects]$ python get-currentdir-with-os/main.py 
+/home/alexkg/workspace/sandbox-projects/python-projects
+"""
