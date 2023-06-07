3b12c8d6fd63557b9262835ad7aaac7dd40da842
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-06-07 09:50:59 -0300

    creating an example of how to use the standard library sys

---
+++ python-projects/getting-args-using-sys/example-without-method.py
@@ -0,0 +1,14 @@
+import sys
+
+all_args = sys.argv
+print(all_args)
+
+name_of_script = sys.argv[0]
+print('Script name :' + name_of_script)
+
+first_arg = sys.argv[1]
+print('My first name :' + first_arg)
+
+second_arg = sys.argv[2]
+print('My last name :' + second_arg)
+
