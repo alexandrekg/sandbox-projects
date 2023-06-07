f15cffe4624bbec9ad4a44ec3ba55ef7728419e0
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-06-07 10:07:03 -0300

    finish example-with-method

--- python-projects/getting-args-using-sys/example-with-method.py
+++ python-projects/getting-args-using-sys/example-with-method.py
@@ -1,3 +1,16 @@
 import sys
 
+
 def main():
+    my_name = sys.argv[1]
+    script_name = sys.argv[0]
+
+    print(f'{script_name} running by {my_name}')
+
+
+main()
+
+"""
+[master][~/workspace/sandbox-projects/python-projects/getting-args-using-sys]$ python example-with-method.py Alexandre
+example-with-method.py running by Alexandre
+"""
