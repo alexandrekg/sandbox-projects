875c84c32a0d45d80ea259d7d7c850983959361b
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-06-10 16:38:01 -0300

    finish calc_age method

--- books/20-libraries-you-arent-using-but-should/common-situation-to-use-a-namedtuple/main.py
+++ books/20-libraries-you-arent-using-but-should/common-situation-to-use-a-namedtuple/main.py
@@ -14,14 +14,18 @@ def main():
 
 
 def _get_normal_tuple_response(name, birth_year, current_year, current_role):
-    age = current_year - birth_year
+    age = _calc_age(birth_year, current_year)
     return name, age, current_role
 
 
 def _get_named_tuple_response(name, birth_year, current_year, current_role):
-    my_age = current_year - birth_year
+    my_age = _calc_age(birth_year, current_year)
     tup = namedtuple('Person', 'name age role')
     return tup(name=name, age=my_age, role=current_role)
+
+
+def _calc_age(birthday_year, current_year):
+    return current_year - birthday_year
 
 
 if __name__ == "__main__":
