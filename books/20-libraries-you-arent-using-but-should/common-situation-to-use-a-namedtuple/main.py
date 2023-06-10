b45521625d1cf321b14ad0c151aad9122c724769
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-06-10 16:33:03 -0300

    creating the basic structure of the script

--- books/20-libraries-you-arent-using-but-should/common-situation-to-use-a-namedtuple/main.py
+++ books/20-libraries-you-arent-using-but-should/common-situation-to-use-a-namedtuple/main.py
@@ -1,5 +1,13 @@
 def main():
-    return True
+    name = "Alex"
+    birth_year = 1999
+    current_year = 2023
+    current_role = 'Developer'
+
+    normal_tuple_response = _get_normal_tuple_response(name, birth_year, current_year, current_role)
+    print(normal_tuple_response)
+    named_tuple_response = _get_named_tuple_response(name, birth_year, current_year, current_role)
+    print(named_tuple_response)
 
 
 if __name__ == "__main__":
