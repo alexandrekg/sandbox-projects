b7f4319f33362e3fd661d96986365fad794b0d18
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-06-10 16:37:01 -0300

    finish namedtuple response logic

--- books/20-libraries-you-arent-using-but-should/common-situation-to-use-a-namedtuple/main.py
+++ books/20-libraries-you-arent-using-but-should/common-situation-to-use-a-namedtuple/main.py
@@ -1,3 +1,6 @@
+from collections import namedtuple
+
+
 def main():
     name = "Alex"
     birth_year = 1999
@@ -13,6 +16,12 @@ def main():
 def _get_normal_tuple_response(name, birth_year, current_year, current_role):
     age = current_year - birth_year
     return name, age, current_role
+
+
+def _get_named_tuple_response(name, birth_year, current_year, current_role):
+    my_age = current_year - birth_year
+    tup = namedtuple('Person', 'name age role')
+    return tup(name=name, age=my_age, role=current_role)
 
 
 if __name__ == "__main__":
