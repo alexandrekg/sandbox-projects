cdf263987f6229caa31d791ab8feb815cd15b322
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-06-13 07:32:37 -0300

    finish exercise

--- python-projects/opening-a-file-with/main.py
+++ python-projects/opening-a-file-with/main.py
@@ -4,9 +4,10 @@ def main():
         file_data = file.readlines()
         total_lines = len(file_data)
         total_words = len([word for row in file_data for word in row.split(" ")])
-        total_characters = len(file_data)
+        total_characters = len("".join(file_data).replace("\n", " "))
         print("Line count: ", total_lines)
-    return True
+        print("Word count: ", total_words)
+        print("Character count: ", total_characters)
 
 
 if __name__ == "__main__":
