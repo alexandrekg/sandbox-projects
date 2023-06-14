9e38716416ec28691f9d2635896d4510ad936936
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-06-14 07:54:17 -0300

    finish example without context_manager

---
+++ books/20-libraries-you-arent-using-but-should/example-with-and-without-context-manager/example-without-context-manager.py
@@ -0,0 +1,23 @@
+from time import perf_counter
+
+
+def timing_func(func):
+    def wrapper(*args, **kwargs):
+        t0 = perf_counter()
+        result = func(*args, **kwargs)
+        t1 = perf_counter()
+        print(f"Execution time: {t1 - t0} seconds")
+        return result
+    return wrapper
+
+
+def my_function():
+    # code to be timed
+    print("Executing my function...")
+    for i in range(1000000):
+        pass
+
+
+if __name__ == "__main__":
+    timed_function = timing_func(my_function)
+    timed_function()
