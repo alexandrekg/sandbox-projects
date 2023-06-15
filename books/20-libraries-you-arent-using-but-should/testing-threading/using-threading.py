c59c5e7a825be835b7a97d1bd934c346f926a0f2
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-06-15 20:15:07 -0300

    not working yet

--- books/20-libraries-you-arent-using-but-should/testing-threading/using-threading.py
+++ books/20-libraries-you-arent-using-but-should/testing-threading/using-threading.py
@@ -8,10 +8,14 @@ def work():
 
 if __name__ == "__main__":
     t0 = time.time()
-    print(work())
+
+    thread = threading.Thread(target=work)
+    thread.start()
+    thread.join()
+
+    result = thread.result()
+    print(result)
+
     t1 = time.time()
     result = t1 - t0
     print(result)
-
-
-print("Result: ", result)
