43ceec24aaa0f916b33dc020a9d74ebfa50af1c9
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-06-15 20:17:53 -0300

    finish testing threading standard module

--- books/20-libraries-you-arent-using-but-should/testing-threading/using-threading.py
+++ books/20-libraries-you-arent-using-but-should/testing-threading/using-threading.py
@@ -2,14 +2,22 @@ import threading
 import time
 
 
-def work():
-    return sum(x for x in range(100))
+class WorkThread(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self._result = None
+
+    def run(self):
+        self._result = sum(x for x in range(100))
+
+    def result(self):
+        return self._result
 
 
 if __name__ == "__main__":
     t0 = time.time()
 
-    thread = threading.Thread(target=work)
+    thread = WorkThread()
     thread.start()
     thread.join()
 
