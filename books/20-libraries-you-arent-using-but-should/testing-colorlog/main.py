c09f7237e67ae8fd131c1af7bc07817fe20db3bc
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-06-27 09:37:43 -0300

    setting up handler

--- books/20-libraries-you-arent-using-but-should/testing-colorlog/main.py
+++ books/20-libraries-you-arent-using-but-should/testing-colorlog/main.py
@@ -3,6 +3,10 @@ import colorlog
 logger = colorlog.getLogger()
 logger.setLevel(colorlog.DEBUG)
 
+handler = colorlog.StreamHandler()
+handler.setFormatter(colorlog.ColoredFormatter())
+logger.addHandler(handler)
+
 
 def main():
     print('Hi')
