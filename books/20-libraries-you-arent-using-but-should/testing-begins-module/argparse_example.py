0e3a15d2629658d5d3d8343e510007a8a34c2563
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-06-28 07:07:19 -0300

    finish example

--- books/20-libraries-you-arent-using-but-should/testing-begins-module/argparse_example.py
+++ books/20-libraries-you-arent-using-but-should/testing-begins-module/argparse_example.py
@@ -8,3 +8,6 @@ def main(a, b):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Add two numbers")
     parser.add_argument('-a', help="First value", type=float, default=0)
+    parser.add_argument('-b', help="Second value", type=float, default=0)
+    args = parser.parse_args()
+    print(main(args.a, args.b))
