323d160336df6648ab5a062cd1cb495bd9c8634c
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-06-28 07:22:40 -0300

    add example

--- books/20-libraries-you-arent-using-but-should/testing-begins-module/begins_example.py
+++ books/20-libraries-you-arent-using-but-should/testing-begins-module/begins_example.py
@@ -6,5 +6,9 @@ def main(a: 'First value' = 0.0, b: 'Second Value' = 0.0):
     """
         I'm not using if __name__ == "__main__": because begins is smart so,
         when we are using begin.start, this will be the starting point of the program
+
+        Example:
+            └> python begins_example.py -a 10 -b 15
+                Result: 25.0
     """
     print(a + b)
