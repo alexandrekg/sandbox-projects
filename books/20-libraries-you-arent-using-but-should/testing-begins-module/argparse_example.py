3eea01bd401ec733e45a247b3675ddd7ef1943a7
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-06-28 07:06:22 -0300

    initial logic

--- books/20-libraries-you-arent-using-but-should/testing-begins-module/argparse_example.py
+++ books/20-libraries-you-arent-using-but-should/testing-begins-module/argparse_example.py
@@ -1,1 +1,10 @@
 import argparse
+
+
+def main(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Add two numbers")
+    parser.add_argument('-a', help="First value", type=float, default=0)
