f2dc1a9c8642bec9cb9d85f3177deeef7c67295e
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-06-29 14:27:03 -0300

    another part of the book example

--- books/20-libraries-you-arent-using-but-should/testing-pywebview/book-example.py
+++ books/20-libraries-you-arent-using-but-should/testing-pywebview/book-example.py
@@ -17,3 +17,23 @@ with doc.head:
     script(src='https://code.jquery.com/jquery-2.1.1.min.js')
     [script(src=bootstrap + 'js/' + x)
      for x in ['bootstrap.min.js', 'bootstrap-dropdown.js']]
+
+with doc:
+    with div(cls='container'):
+        with h1():
+            span(cls='glyphicon glyphicon-map-marker') span('My Heading')
+        with div(cls='row'):
+            with div(cls='col-sm-6'):
+                p('{{body}}')
+            with div(cls='col-sm-6'):
+                p('Evaluate an expression:')
+                input_(id='expression', type='text')
+                button('Evaluate', cls='btn btn-primary')
+                div(style='margin-top: 10px;')
+                with div(cls='dropdown'):
+                    with button(cls="btn btn-default dropdown-toggle",
+                                type="button", data_toggle='dropdown'):
+    span('Dropdown') span(cls='caret')
+    items = ('Action', 'Another action',
+    'Yet another action')
+    ul((li(a(x, href='#')) for x in items), cls='dropdown-menu')
