d7f92c8d7481816ac346823966f385adaa3a776b
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-06-29 14:17:54 -0300

    add more imports

--- books/20-libraries-you-arent-using-but-should/testing-pywebview/main.py
+++ books/20-libraries-you-arent-using-but-should/testing-pywebview/main.py
@@ -2,7 +2,8 @@ import webview
 import dominate
 from string import ascii_letters
 from random import choice, randint
-
+from dominate.tags import *
+from bottle import route, run, template
 
 def root():
     print('Hi')
