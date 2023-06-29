69d1b56c164c83ec96a38b41e28fd5e27c6ca1f0
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-06-29 14:18:38 -0300

    add bootstrap cdn

--- books/20-libraries-you-arent-using-but-should/testing-pywebview/main.py
+++ books/20-libraries-you-arent-using-but-should/testing-pywebview/main.py
@@ -5,6 +5,10 @@ from random import choice, randint
 from dominate.tags import *
 from bottle import route, run, template
 
+bootstrap = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/'
+bootswatch = 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/'
+
+
 def root():
     print('Hi')
 
