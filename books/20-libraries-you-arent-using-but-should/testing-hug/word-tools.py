13f2c2b944758113ea8fd1c10d0d643811502d92
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-06-30 09:40:10 -0300

    creating endpoints

--- books/20-libraries-you-arent-using-but-should/testing-hug/word-tools.py
+++ books/20-libraries-you-arent-using-but-should/testing-hug/word-tools.py
@@ -1,2 +1,23 @@
 import hug
 import inflect
+engine = inflect.engine()
+
+
+def singular(word: hug.types.text):
+    """Return the singular version of the word"""
+    return engine.singular_noun(word).lower()
+
+
+def plural(word: hug.types.text):
+    """Return the plural version of the word"""
+    return engine.plural(word).lower()
+
+
+def singular(word: hug.types.text):
+    """Return the singular of word, preserving case"""
+    return engine.singular_noun(word)
+
+
+def plural(word: hug.types.text):
+    """Return the plural of word, preserving case"""
+    return engine.plural(word)
