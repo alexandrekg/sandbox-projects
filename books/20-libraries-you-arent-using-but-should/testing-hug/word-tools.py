a5d867ead297e03813bc019d3b3b770ca664e68c
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-06-30 09:45:12 -0300

    add curl examples

--- books/20-libraries-you-arent-using-but-should/testing-hug/word-tools.py
+++ books/20-libraries-you-arent-using-but-should/testing-hug/word-tools.py
@@ -25,3 +25,20 @@ def singular(word: hug.types.text):
 def plural(word: hug.types.text):
     """Return the plural of word, preserving case"""
     return engine.plural(word)
+
+
+
+"""
+    Examples:
+    ┌[alexkg☮DesktopAlexKG]-(~)
+    └> curl "127.0.0.1:8000/v1/singular/?word=Silly%20Walks"
+    "silly walk"%   
+    
+    ┌[alexkg☮DesktopAlexKG]-(~)
+    └> curl "127.0.0.1:8000/v1/singular/?word=Crisis"
+    "crisi"%
+    
+    ┌[alexkg☮DesktopAlexKG]-(~)
+    └> curl "127.0.0.1:8000/v1/plural/?word=Crisis"
+    "crises"% 
+"""
