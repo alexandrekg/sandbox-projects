5c1bcbdeecaf687252e5e7fe8a2cc39846d884f5
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-07-01 17:50:49 -0300

    finish aware example

--- books/20-libraries-you-arent-using-but-should/testing-datetime-aware-objects/main.py
+++ books/20-libraries-you-arent-using-but-should/testing-datetime-aware-objects/main.py
@@ -4,7 +4,8 @@ from datetime import datetime
 
 def main():
     timezone = pytz.timezone('America/New_York')
-    aware_datetime = timezone.localize()
+    aware_datetime = timezone.localize(datetime(2023, 7, 1, 12, 0, 0))
+    print(aware_datetime)
 
 
 if __name__ == "__main__":
