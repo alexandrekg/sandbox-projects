6846728bd414b6c1d02d77f6eee29a960de04357
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-07-01 17:58:45 -0300

    finish calc with naive datetime

--- books/20-libraries-you-arent-using-but-should/testing-datetime-naive-objects/main.py
+++ books/20-libraries-you-arent-using-but-should/testing-datetime-naive-objects/main.py
@@ -6,5 +6,15 @@ def naive_date():
     print(naive_datetime)
 
 
+def calc_with_naive_datetime():
+    print('----------------------')
+    print('Calc with naive datetime')
+    naive_datetime = datetime(2023, 7, 1, 12, 0, 0)
+    print('Before         ', naive_datetime)
+    new_naive_datetime = naive_datetime + timedelta(hours=3)
+    print('After          ', new_naive_datetime)
+
+
 if __name__ == "__main__":
     naive_date()
+    calc_with_naive_datetime()
