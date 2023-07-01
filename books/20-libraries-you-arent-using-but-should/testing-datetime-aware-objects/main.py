a60d031470bcbc400de9513284023c18857f2c35
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-07-01 17:52:47 -0300

    creating calc example with aware objects

--- books/20-libraries-you-arent-using-but-should/testing-datetime-aware-objects/main.py
+++ books/20-libraries-you-arent-using-but-should/testing-datetime-aware-objects/main.py
@@ -8,5 +8,15 @@ def main():
     print(aware_datetime)
 
 
+def calc_with_timezone_example():
+    timezone = pytz.timezone('America/New_York')
+    aware_datetime = timezone.localize(datetime(2023, 7, 1, 12, 0, 0))
+    print(aware_datetime)
+
+
 if __name__ == "__main__":
+    print('Aware object example')
     main()
+    print('-----------------------')
+    print('Calculating dates and times with aware objects')
+    calc_with_timezone_example()
