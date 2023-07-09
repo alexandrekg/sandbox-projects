7e227855139a554ec968e9e9a1c422b4026b7e36
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-07-09 12:21:51 -0300

    this example is not working, and idk why

--- books/20-libraries-you-arent-using-but-should/testing-parsedatetime/main.py
+++ books/20-libraries-you-arent-using-but-should/testing-parsedatetime/main.py
@@ -13,6 +13,12 @@ def main():
         "7/16/16",
     ]
 
+    print('{:30s}{:>30s}'.format('Input', 'Result'))
+    print('=' * 60)
+    for e in examples:
+        dt, result = cal.parseDT(e)
+        print('{:30s}{:>30s}'.format('"' + e + '"', dt.ctime()))
+
 
 if __name__ == "__main__":
     main()
