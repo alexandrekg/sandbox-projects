72d2289bc44209bac54ddf5e3fc76d92aa12bf50
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-07-11 07:47:47 -0300

    finish my version of binary_search

--- books/grokking-algorithms-an-illustrated-guide-for-programmers-and-other-curious-people/binary_search/code_example/main.py
+++ books/grokking-algorithms-an-illustrated-guide-for-programmers-and-other-curious-people/binary_search/code_example/main.py
@@ -1,24 +1,24 @@
 
 def binary_search(sorted_array, item):
     low = 0
-    high = len(sorted_array) - 1 # 4
-    
-    while low <= high:    
+    high = len(sorted_array) - 1
+    total_guesses = 0
+    right_guess = 0
+    while low <= high:
         mid = round((low + high) / 2)
-        print("mid: ", mid)
         guess = sorted_array[mid]
         if guess < item:
             low = mid + 1
-            print(low)
         if guess > item:
             high = mid - 1
-            print(high)
         if guess == item:
-            return mid    
-        else:
-            return None
+            right_guess = guess
+            break
+
+        total_guesses += 1    
+    print(f'Total guesses: {total_guesses}, guess number is: {right_guess}')
 
 
 if __name__ == "__main__":
-    num_array = [1, 3, 5, 7, 9]
-    print(binary_search(num_array, 3))
+    num_array = [i for i in range(101)]
+    binary_search(num_array, 99)
