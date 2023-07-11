ff1f3f9f6ee9f9b44a670e7d7f62e50889ee5f49
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-07-11 07:26:54 -0300

    trying implement myself

--- books/grokking-algorithms-an-illustrated-guide-for-programmers-and-other-curious-people/binary_search/code_example/main.py
+++ books/grokking-algorithms-an-illustrated-guide-for-programmers-and-other-curious-people/binary_search/code_example/main.py
@@ -2,7 +2,16 @@
 def binary_search(sorted_array, item):
     low = 0
     high = len(list) - 1
+    mid = (low + high) / 2
+    guess = list[mid]
+    total_guesses = 0
+    
+    if guess < item:
+        low = mid + 1
+        
+    print('Total guesses: ', total_guesses)
 
 
 if __name__ == "__main__":
-    binary_search()
+    num_array = [i for i in range(101)]
+    binary_search(num_array, 99)
