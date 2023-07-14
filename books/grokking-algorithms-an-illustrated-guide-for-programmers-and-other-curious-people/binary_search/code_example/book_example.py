3176f7dc60304ae15b956c626fae7b4c14ecdfe3
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-07-14 09:21:43 -0300

    finish example by the book

--- books/grokking-algorithms-an-illustrated-guide-for-programmers-and-other-curious-people/binary_search/code_example/book_example.py
+++ books/grokking-algorithms-an-illustrated-guide-for-programmers-and-other-curious-people/binary_search/code_example/book_example.py
@@ -1,6 +1,20 @@
 def binary_search(my_list, item):
     low = 0
+    high = len(my_list) - 1
     
+    while low <= high:
+        mid = (low + high)
+        guess = list(mid)
+        
+        if guess == item:
+            return mid
+        if guess > item:
+            return mid - 1
+        else:
+            low = mid + 1
+
+    return None
+
     
 if __init__ == "__main__":
     binary_search()
