98061cace7415cb3a6142535607bd8c95642f58b
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2024-05-14 23:17:01 -0300

    <Python-Django Complete Course> - working with templates

--- python-django-complete-course/devsearch/projects/views.py
+++ python-django-complete-course/devsearch/projects/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def projects(request):
-    return HttpResponse('Here are our projects')
+    return render(request, 'projects.html')
     
 def project(request, pk):
-    return HttpResponse('Single project!' + ' ' + str(pk))
+    return render(request, 'single-project.html')
